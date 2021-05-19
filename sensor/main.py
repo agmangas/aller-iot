1dadff262d7b2eb2bef77de26f46c5d8c5398934
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-05-19 18:55:07 +0200

    Move to separate folder

--- sensor/main.py
+++ sensor/main.py
@@ -1,10 +1,11 @@
 import gc
+import socket
 import time
 
 import machine
 import utime
 from LIS2HH12 import LIS2HH12
-from network import WLAN
+from network import WLAN, LoRa
 from pycoproc_1 import Pycoproc
 
 WLAN_SSID = "pycom-wifi"
@@ -46,21 +47,38 @@ def connect_wlan(wlan):
     print("wlan.ifconfig:\n{}".format(wlan.ifconfig()))
 
 
+def log_acc(acc):
+    print("Acceleration: " + str(acc.acceleration()))
+    print("Roll: " + str(acc.roll()))
+    print("Pitch: " + str(acc.pitch()))
+
+
+def init_lora():
+    print("Initializing LoRa")
+    lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+    sckt = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    sckt.setblocking(False)
+
+    return lora, sckt
+
+
 def main():
     enable_gc()
+
     py = Pycoproc(Pycoproc.PYTRACK)
     acc = LIS2HH12()
     
     wlan = WLAN(mode=WLAN.STA)
     connect_wlan(wlan=wlan)
 
+    lora, sckt = init_lora()
+
     rtc = machine.RTC()
     setup_rtc(rtc=rtc)
 
     while True:
-        print("Acceleration: " + str(acc.acceleration()))
-        print("Roll: " + str(acc.roll()))
-        print("Pitch: " + str(acc.pitch()))
-        time.sleep(1.0)
+        log_acc(acc=acc)
+        sckt.send("Hello LoRa")
+        time.sleep(2.0)
 
 main()
