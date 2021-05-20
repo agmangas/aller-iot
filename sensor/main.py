45305aab2e2020f70e54b3ce04f269f92bde8d98
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-05-20 17:40:42 +0200

    Do not connect to WLAN

--- sensor/main.py
+++ sensor/main.py
@@ -6,12 +6,9 @@ import machine
 import pycom
 import utime
 from LIS2HH12 import LIS2HH12
-from network import WLAN, LoRa
+from network import LoRa
 from pycoproc_1 import Pycoproc
 
-WLAN_SSID = "pycom-wifi"
-WLAN_PASS = "securepassword"
-WLAN_TIMEOUT_MS = 20000
 
 def enable_gc():
     print("Enabling garbage collector")
@@ -25,31 +22,6 @@ def setup_rtc(rtc):
     rtc.ntp_sync("pool.ntp.org")
     utime.sleep_ms(750)
     print("RTC set from NTP to UTC: {}".format(rtc.now()))
-
-
-def connect_wlan(wlan):
-    print("Connecting to SSID: {}".format(WLAN_SSID))
-
-    wlan.connect(
-        ssid=WLAN_SSID, 
-        auth=(WLAN.WPA2, WLAN_PASS), 
-        timeout=WLAN_TIMEOUT_MS)
-
-    now = time.ticks_ms()
-
-    while not wlan.isconnected():
-        if (time.ticks_ms() - now) > WLAN_TIMEOUT_MS:
-            raise TimeoutError("WiFi timeout")
-
-        print("Still disconnected")
-        pycom.rgbled(0xFF0000)
-        time.sleep(0.5)
-        pycom.rgbled(0x000000)
-        time.sleep(0.5)
-
-    pycom.rgbled(0x000000)
-    print("WiFi connected succesfully")
-    print("wlan.ifconfig:\n{}".format(wlan.ifconfig()))
 
 
 def log_acc(acc):
@@ -70,14 +42,9 @@ def init_lora():
 def main():
     pycom.heartbeat(False)
     enable_gc()
-    py = Pycoproc(Pycoproc.PYTRACK)
+    pycoproc = Pycoproc(Pycoproc.PYTRACK)
     acc = LIS2HH12()
-    
-    wlan = WLAN(mode=WLAN.STA)
-    connect_wlan(wlan=wlan)
-
     lora, sckt = init_lora()
-
     rtc = machine.RTC()
     setup_rtc(rtc=rtc)
 
@@ -90,5 +57,6 @@ def main():
         time.sleep(0.5)
         pycom.rgbled(0x000000)
         time.sleep(0.5)
+
 
 main()
